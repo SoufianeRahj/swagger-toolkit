49813ae0a6ce1561d8ebc6b83ee1036ab2c8c2c0
Soufiane RAHJ <soufiane.rahj@instant-system.com>
2023-03-01 16:19:54 +0100

    first working version

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const getAllDependencies = require("./match.js");
+
+// read the input swagger
+const swaggerObj = fs.readFileSync("./../swagger-input.json");
+const swagger = JSON.parse(swaggerObj);
+
+// extract the models from the swagger
+const { definitions } = swagger;
+
+// export the model in a separated file
+const exportModel = (root) => {
+  const dependencies = getAllDependencies(definitions, root);
+  const exportedModel = {};
+  for (let model of dependencies) {
+    exportedModel[model] = JSON.parse(JSON.stringify(definitions[model]));
+  }
+  const jsonModel = JSON.stringify(exportedModel);
+  fs.writeFileSync(`./../${root}.json`, jsonModel, {
+    encoding: "utf8",
+    flag: "w",
+  });
+};
+
+const main = () => {
+  const root = process.argv[2];
+  if (!root) {
+    console.log("please enter an argument");
+    return 1;
+  }
+  if (!definitions[root]) {
+    console.log("the element entered is not part of the swagger");
+    return 1;
+  }
+  exportModel(root);
+  return 0;
+};
+
+main();
---
+++ match.js
@@ -0,0 +1,65 @@
+const regex = /"#\/definitions\/[A-z]*"/g;
+
+const getModelFromPattern = (pattern) => {
+  return pattern.substring(15, pattern.length - 1);
+};
+
+const getDependency = (collection, modelObj) => {
+  const model = JSON.stringify(modelObj);
+  const results = model.match(regex);
+  if (results) {
+    const dependencies = results.map(getModelFromPattern);
+    return dependencies;
+  }
+  return null;
+};
+
+const getAllDependencies = (collection, root) => {
+  //contains the list of dependencies for the root model passed
+  const dependenciesList = new Set();
+  // the dependecies list contains the root node as well
+  dependenciesList.add(root);
+
+  // add a set for the nodes for which the dependencies were searched
+  const searchedItems = new Set();
+
+  // contains the nodes that will be traversed with a BFT algo
+  let currentNodes = [];
+  let newNodes = [];
+
+  // first iteration
+  const results = getDependency(collection, collection[root]);
+  searchedItems.add(root);
+  if (results) {
+    results.forEach((el) => {
+      dependenciesList.add(el);
+      newNodes.push(el);
+    });
+  }
+
+  currentNodes = newNodes;
+  newNodes = []; // the ref remains only in current nodes
+
+  while (dependenciesList.size !== searchedItems.size) {
+    //search for the dependencies of the current nodes
+    for (let node of currentNodes) {
+      // only search for dependencies of unsearched items
+      if (!searchedItems.has(node)) {
+        const foundNodes = getDependency(collection, collection[node]);
+        // add the searched node
+        searchedItems.add(node);
+        if (foundNodes) {
+          foundNodes.forEach((el) => {
+            dependenciesList.add(el);
+            newNodes.push(el);
+          });
+        }
+      }
+    }
+    currentNodes = newNodes;
+    newNodes = [];
+  }
+  return dependenciesList;
+};
+
+module.exports = getAllDependencies;
